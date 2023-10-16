c327d650fe4ade03451edf488b46b38d0554ae67
Faris Putra <farisp1212@gmail.com>
2023-10-17 03:12:26 +0700

    Update MainActivity

--- app/src/main/java/com/example/courseapp/MainActivity.kt
+++ app/src/main/java/com/example/courseapp/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.courseapp.model.CourseRepository
 import com.example.courseapp.ui.theme.CourseAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +27,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CourseListApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CourseListApp() {
+    Scaffold (
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        val courses = CourseRepository.courses
+        CoursesLazyGrid(
+            courses = courses,
+            contentPadding = it
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name)
+            )
+        },
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CourseAppPreview() {
     CourseAppTheme {
-        Greeting("Android")
+        CourseListApp()
     }
 }
